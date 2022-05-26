7348b28c9f4bee92129df200e953049bd6e7209a
unknown <pharo.md@gmail.com>
2022-05-26 10:38:54 +0200

    update to task0.6

--- task0_6.py
+++ task0_6.py
@@ -1,12 +1,12 @@
 
-def maximum(x,y,z):
-    if x > y and x > z:
-        return x
-    elif y >x and y > z :
-        return y
-    elif z >x and z >y :
-        return z
+def maximum(number1,number2,number3):
+    if number1 > number2 and number1 > number3:
+        return number1
+    elif number2 >number1 and number2 > number3 :
+        return number2
+    elif number3 >number1 and number3 >number2 :
+        return number3
     else : #execute the else part only if some numbers are equal
         return 0 
 
-print(str(maximum(1,8,7)))
+print(str(maximum(1,8,8)))
