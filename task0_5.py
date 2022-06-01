8d4cbb2140d5bbaf8679e0b60ad658e525a772f9
unknown <pharo.md@gmail.com>
2022-06-01 09:01:09 +0200

    update to task5

--- task0_5.py
+++ task0_5.py
@@ -1,7 +1,7 @@
 
 def area_of_triangle(value1,value2,value3):
-    semi1 = (1/2) * (value3+ value2 + value1)
-    area = (semi1*((semi1 - value1)*(semi1-value2)*(semi1-value3)))**(1/2)
+    semiperimeter = (1/2) * (value3+ value2 + value1)
+    area = (semiperimeter*((semiperimeter - value1)*(semiperimeter-value2)*(semiperimeter-value3)))**(1/2)
     return area
 
 area_of_triangle(3,4,5)
