21d40a96abf900e76b31c6f80fcd665f84b07fb1
unknown <pharo.md@gmail.com>
2022-06-03 12:24:47 +0200

    update to task

--- task0_7.py
+++ task0_7.py
@@ -1,11 +1,11 @@
 def celsius_to_fahrenheit(temp):
     fahrenheit = (temp * 1.8) +32
-    return ( f"{temp} degree celsius in fahrenheit is: {fahrenheit}")
+    return ( fahrenheit)
 
 celsius_to_fahrenheit(1)
 
 def fahrenheit_to_celsius(temp):
     celsius = (temp - 32) /1.8
-    return (f"{temp} fahrenheit in celsius: {round(celsius)}"  )
+    return (round(celsius) )
 
 fahrenheit_to_celsius(33.8)
